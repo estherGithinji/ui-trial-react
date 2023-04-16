e2bd46b3888a4a3d6fb0261675f0fbcc4c8015d4
esther <esther.githinji@student.moringaschool.com>
2023-04-16 16:59:33 +0300

    initial commit

---
+++ ireporter/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import AddReport from './users/AddReport';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1> */}
+
+      {/* <Navbar/> */}
+      {/* <Signup/> */}
+      {/* <Login/> */}
+      <AddReport/>
+    </div>
+  );
+}
+
+export default App;
---
+++ ireporter/src/users/Navbar.jsx
@@ -0,0 +1,152 @@
+import React from 'react'
+import { Disclosure } from '@headlessui/react'
+import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
+
+
+const user = {
+    name: 'Tom Cook',
+    email: 'tom@example.com',
+    imageUrl:
+      'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+}
+const navigation = [
+    { name: 'Home', href: '#', current: false },
+    { name: 'Public Reports', href: '#', current: true },
+    { name: 'About Us', href: '#', current: false },
+    { name: 'Admin', href: '#', current: false },
+]
+const userNavigation = [
+    { name: 'Your Profile', href: '#' },
+    { name: 'Settings', href: '#' },
+    { name: 'Sign out', href: '#' },
+]
+  
+function classNames(...classes) {
+    return classes.filter(Boolean).join(' ')
+}
+
+function Navbar() {
+  return (
+    <Disclosure as="nav" className="bg-gray-800">
+    {({ open }) => (
+      <>
+        <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+          <div className="flex h-16 justify-between">
+            <div className="flex">
+              <div className="-ml-2 mr-2 flex items-center md:hidden">
+                {/* Mobile menu button */}
+                <Disclosure.Button className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                  <span className="sr-only">Open main menu</span>
+                  {open ? (
+                    <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
+                  ) : (
+                    <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
+                  )}
+                </Disclosure.Button>
+              </div>
+              <div className="flex flex-shrink-0 items-center">
+                <img
+                  className="block h-8 w-auto lg:hidden"
+                  src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
+                  alt="Your Company"
+                />
+                <img
+                  className="hidden h-8 w-auto lg:block"
+                  src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
+                  alt="Your Company"
+                />
+              </div>
+              <div className="hidden md:ml-6 md:flex md:items-center md:space-x-4">
+                {navigation.map((item) => (
+                  <a
+                    key={item.name}
+                    href={item.href}
+                    className={classNames(
+                      item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                      'px-3 py-2 rounded-md text-sm font-medium'
+                    )}
+                    aria-current={item.current ? 'page' : undefined}
+                  >
+                    {item.name}
+                  </a>
+                ))}
+              </div>
+            </div>
+
+            <div className="flex items-center">
+              <div className="flex-shrink-0">
+                <button
+                  type="button"
+                  className="relative inline-flex items-center rounded-md border border-transparent bg-indigo-500 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-600 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-gray-800"
+                >
+                  <span>Sign Up</span>
+                </button>
+              </div>
+              <div className="flex-shrink-0 ml-2">
+                <button
+                  type="button"
+                  className="relative inline-flex items-center rounded-md border border-transparent bg-indigo-500 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-600 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-gray-800"
+                >
+                  <span>Log In</span>
+                </button>
+              </div>
+            </div>
+
+          </div>
+        </div>
+
+        <Disclosure.Panel className="md:hidden">
+          <div className="space-y-1 px-2 pt-2 pb-3 sm:px-3">
+            {navigation.map((item) => (
+              <Disclosure.Button
+                key={item.name}
+                as="a"
+                href={item.href}
+                className={classNames(
+                  item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                  'block px-3 py-2 rounded-md text-base font-medium'
+                )}
+                aria-current={item.current ? 'page' : undefined}
+              >
+                {item.name}
+              </Disclosure.Button>
+            ))}
+          </div>
+          <div className="border-t border-gray-700 pt-4 pb-3">
+            <div className="flex items-center px-5 sm:px-6">
+              <div className="flex-shrink-0">
+                <img className="h-10 w-10 rounded-full" src={user.imageUrl} alt="" />
+              </div>
+              <div className="ml-3">
+                <div className="text-base font-medium text-white">{user.name}</div>
+                <div className="text-sm font-medium text-gray-400">{user.email}</div>
+              </div>
+              <button
+                type="button"
+                className="ml-auto flex-shrink-0 rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+              >
+                <span className="sr-only">View notifications</span>
+                <BellIcon className="h-6 w-6" aria-hidden="true" />
+              </button>
+            </div>
+            <div className="mt-3 space-y-1 px-2 sm:px-3">
+              {userNavigation.map((item) => (
+                <Disclosure.Button
+                  key={item.name}
+                  as="a"
+                  href={item.href}
+                  className="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white"
+                >
+                  {item.name}
+                </Disclosure.Button>
+              ))}
+            </div>
+          </div>
+        </Disclosure.Panel>
+      </>
+    )}
+  </Disclosure>
+  )
+}
+
+export default Navbar
